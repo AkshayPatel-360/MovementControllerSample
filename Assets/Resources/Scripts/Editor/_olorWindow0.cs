22ae7dded28a73d39dbd89a8c1adf91d4ef50399
AkshayPatel-360 <67767059+AkshayPatel-360@users.noreply.github.com>
2020-12-08 16:18:23 -0500

    Random Colour Exercise Complete

--- Assets/Resources/Scripts/Editor/_olorWindow0.cs
+++ Assets/Resources/Scripts/Editor/_olorWindow0.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;  //Required for MenuItem, means that this is an Editor script, must be placed in an Editor folder, and cannot be compiled!
@@ -12,18 +14,19 @@ public class ColorWindow : EditorWindow
     {
         EditorWindow window = GetWindow<ColorWindow>("Color Window");
     }
-    private int r;
+
     private Color[] colors;
     private int width = 8;
     private int height = 8;
+    float randomnessInColour;
+    float randomColour;
     Texture colorTexture;
     Renderer textureTarget;
-    private int random;
-    private float randomNessInFloat;
+
     Color selectedColor = Color.white;
     Color eraseColor = Color.white;
-    Color paintColor = Color.white;
-    Rect colorRect;
+    Color tempSelectedColour;
+
 
     public void OnEnable()
     {
@@ -40,57 +43,28 @@ public class ColorWindow : EditorWindow
 
     void OnGUI() //Called every frame in Editor window
     {
-        GUILayout.BeginHorizontal();
-        //Have each element below be side by side
+        GUILayout.BeginHorizontal();        //Have each element below be side by side
         DoControls();
         DoCanvas();
-        // DoPainting();
         GUILayout.EndHorizontal();
     }
-    //void DoPainting()
-    //{
 
-
-    //    Event myEvent = Event.current;
-
-    //    for (int i = 0; i < width; i++)
-    //    {
-
-    //        for (int j = 0; j < height; j++)
-    //        {
-
-    //            if ( colorRect.Contains(myEvent.mousePosition))
-    //            {
-    //                //if (Mathf.Approximately(colors[i].r, colors[i + 1].r) || Mathf.Approximately(colors[i].g, colors[i + 1].g) || Mathf.Approximately(colors[i].b, colors[i + 1].b))
-    //                ////{
-    //                //    if(myEvent.button == 0)
-    //                //    {
-    //                        colors[i] = paintColor;
-
-    //                //}
-
-    //                //}
-    //                myEvent.Use();
-    //            }
-    //        }
-    //    }
-    //}
     void DoControls()
     {
         GUILayout.BeginVertical();                                                      //Start vertical section, all GUI draw code after this will belong to same vertical
         GUILayout.Label("ToolBar", EditorStyles.largeLabel);                            //A label that says "Toolbar"
         selectedColor = EditorGUILayout.ColorField("Paint Color", selectedColor);       //Make a color field with the text "Paint Color" and have it fill the selectedColor var
         eraseColor = EditorGUILayout.ColorField("Erase Color", eraseColor);             //Make a color field with the text "Erase Color"
+
+       
+
         if (GUILayout.Button("Fill All"))                                               //A button, if pressed, returns true
             colors = colors.Select(c => c = selectedColor).ToArray();                   //Linq expresion, for every color in the color array, sets it to the selected color
-        GUILayout.Label("Randomness ", EditorStyles.boldLabel);
-        randomNessInFloat = EditorGUILayout.FloatField(randomNessInFloat);
-        if (GUILayout.Button("Apply Randomness"))
-        {
-            Randomness();
-        }
-        GUILayout.Label("Paint color", EditorStyles.centeredGreyMiniLabel);
-        paintColor = EditorGUILayout.ColorField("Paint color", paintColor);
+
+        GUILayout.Label("Randomness In Colour");
+        randomnessInColour = EditorGUILayout.FloatField(randomnessInColour);
+
+
         GUILayout.FlexibleSpace();                                                      //Flexible space uses any left over space in the loadout
         textureTarget = EditorGUILayout.ObjectField("Output Renderer", textureTarget, typeof(Renderer), true) as Renderer;  //Build an object field that accepts a renderer
 
@@ -114,53 +88,38 @@ public class ColorWindow : EditorWindow
         GUILayout.EndVertical();                                                        //end vertical section
     }
 
-    private void Randomness()
-    {
-        for (int i = 0; i < colors.Count(); i++)
-        {
-            random = Random.Range(0, 2);
-            if (random == 0)
-            {
-                colors[i] = new Color(colors[i].r + randomNessInFloat, colors[i].g + randomNessInFloat, colors[i].b + randomNessInFloat);
-            }
-            else
-            {
-                colors[i] = new Color(colors[i].r - randomNessInFloat, colors[i].g - randomNessInFloat, colors[i].b - randomNessInFloat);
-            }
-
-        }
-    }
-
     void DoCanvas()
     {
         Event evt = Event.current;                     //Grab the current event
 
         Color oldColor = GUI.color;                    //GUI color uses a static var, need to save the original to reset it
-        GUILayout.BeginHorizontal();
-        //All following gui will be on one horizontal line until EndHorizontal is called
+        GUILayout.BeginHorizontal();                   //All following gui will be on one horizontal line until EndHorizontal is called
         for (int i = 0; i < width; i++)
         {
             GUILayout.BeginVertical();                //All following gui will be in a vertical line
             for (int j = 0; j < height; j++)
             {
                 int index = j + i * height;           //Rememeber, this is just like a 2D array, but in 1D
-                colorRect = GUILayoutUtility.GetRect(GUIContent.none, GUIStyle.none, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)); //Reserve a square, which will autofit to the size given
+                Rect colorRect = GUILayoutUtility.GetRect(GUIContent.none, GUIStyle.none, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)); //Reserve a square, which will autofit to the size given
                 if ((evt.type == EventType.MouseDown || evt.type == EventType.MouseDrag) && colorRect.Contains(evt.mousePosition)) //Can now paint while dragging update
                 {
-                    if (evt.button == 0)
-                    {
-                        colors[index] = selectedColor;
+                    tempSelectedColour = selectedColor;
+                    randomColour = Random.Range(-randomnessInColour, randomnessInColour);
+                    randomColour = Mathf.Clamp(randomColour, -0.5f, 0.5f);
+                    selectedColor.r = selectedColor.r + randomColour;
+                    selectedColor.g = selectedColor.g + randomColour;
+                    selectedColor.b = selectedColor.b + randomColour;
 
-
-                    }
-                    if (evt.button == 1)
+                    if (evt.button == 0)                //If mouse button pressed is left
                     {
-                        FillNewColor(i, j, selectedColor);
+                        colors[index] = selectedColor; //Set the color of the index
+                        selectedColor = tempSelectedColour;
                     }
                     else
                         colors[index] = eraseColor;   //Set the color of the index
                     evt.Use();                        //The event was consumed, if you try to use event after this, it will be non-sensical
                 }
+
                 GUI.color = colors[index];            //Same as a 2D array
                 GUI.DrawTexture(colorRect, colorTexture); //This is colored by GUI.Color!!!
             }
@@ -168,27 +127,5 @@ public class ColorWindow : EditorWindow
         }
         GUILayout.EndHorizontal();                    //End horizontal zone
         GUI.color = oldColor;                         //Restore the old color
-    }
-
-
-    private void FillNewColor(int x, int y, Color lastColor)
-    {
-        int current = y + x * height;
-
-        if (x < 0 || x >= width || y < 0 || y >= height)
-            return;
-
-        if (!colors[current].Equals(lastColor))
-            return;
-
-        if (colors[current].Equals(paintColor))
-            return;
-
-        colors[current] = paintColor;
-
-        FillNewColor(x + 1, y, lastColor);
-        FillNewColor(x - 1, y, lastColor);
-        FillNewColor(x, y + 1, lastColor);
-        FillNewColor(x, y - 1, lastColor);
     }
 }
